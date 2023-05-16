dfdb3145722b532c23b7042842719de6e5a4085e
RuiVelho <ruimv93@gmail.com>
2023-05-16 12:09:00 +0100

    rest of work

--- Streamlit_page.py
+++ Streamlit_page.py
@@ -4,15 +4,68 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+titles=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/movies_noexplode.csv') #Sentiment analysis
+#tiltes=titles[titles['tconst', 'primaryTitle', 'genre', 'compound')]]
+
+directors=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/imbdir_rating.csv')
+directors=directors.sort_values(by="dir_rating", ascending=False).head(10)
+directors=directors.reset_index()
+directors=directors.drop(['index'], axis=1)
+
+actors=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/actors_top10.csv')
+
+genres_analysis=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/genres_analysis.csv')
+
+
 st.set_page_config(page_title='Prediction of Futeture Movies',
                    page_icon=":movie_camera:")
 
-st.title('Prediction of review for the future Movies')
+dash = st.sidebar.radio(
+    "Prediction of review for the future Movies",
+    ('Titles Sentiment Analysis', 'Movies Statistics', 'How good will your movie be?'))
 
-st.sidebar.header('Predict Your next Movies Selection')
+if dash == 'Titles Sentiment Analysis':
+    st.title('Titles Sentiment Analysis')
+    titles
+    
 
-director = st.sidebar.multiselect('Select the Director: ', options=[1,2,3,4,5,6])
+if dash == 'Movies Statistics':
+    col1, col2 = st.columns(2)
+    with col1:
+        st.title('Top 10 Directors')
+        directors
+    
+    with col2:
+        st.title('Top 10 Actors')
+        actors
+    
+    st.title('Genres Analysis')
+    genres_analysis
 
-actors = st.sidebar.multiselect('Select the Actors: ', options=[1,2,3,4,5,6])
+if dash == 'How good will your movie be?':
 
-category = st.sidebar.multiselect('Select the category of movie: ', options=[1,2,3,4,5,6])
+    st.title('Prediction of review for the future Movies')
+
+    st.sidebar.header('Predict Your next Movies Selection')
+
+    director = st.sidebar.selectbox('Select the Director: ', options=[1,2,3,4,5,6])
+
+    actors = st.sidebar.multiselect('Select the Actors: ', options=[1,2,3,4,5,6])
+    
+    year= ('Select the Actors: ', options=[2002,2023])
+
+    category = st.sidebar.selectbox('Select the category of movie: ', options=genres_analysis['genre'])
+       # if category == "Drama":
+             # genres_df
+  
+
+
+    #    pages = {
+#        "Data Analysis": page_plot1,    #names will change, don't worry :)
+#        "Algorithm": page_plot2     #names will change, don't worry :)
+#        }
+#    selected_page = st.selectbox(
+ #       "Choose Page",
+#        pages.keys()
+        
+        
