580d6eac7364feb497da4edd3896e103989cef25
Bengü Baştuğ <bengubastug@gmail.com>
2023-05-16 17:14:48 +0300

    final changes

--- streamlit_test.py
+++ streamlit_test.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 titles=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/movies_noexplode.csv') #Sentiment analysis
+tiltes1=titles.drop(['titleType','originalTitle','isAdult','endYear','runtimeMinutes','numVotes','Titles'], axis=1)#['tconst', 'primaryTitle', 'genres', 'compound']
 #tiltes=titles[titles['tconst', 'primaryTitle', 'genre', 'compound')]]
 #getting dir names
 dir_names_df = pd.read_csv("https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/pivot_dir.csv")
@@ -33,11 +34,11 @@ st.set_page_config(page_title='Prediction of Futeture Movies',
 
 dash = st.sidebar.radio(
     "Prediction of review for the future Movies",
-    ('Titles Sentiment Analysis', 'Movies Statistics', 'Actors', 'How good will your movie be?'))
+    ('Titles Sentiment Analysis', 'Movies Statistics', 'Actors'))
 
 if dash == 'Titles Sentiment Analysis':
     st.title('Titles Sentiment Analysis')
-    titles
+    tiltes1
     
 
 if dash == 'Movies Statistics':
