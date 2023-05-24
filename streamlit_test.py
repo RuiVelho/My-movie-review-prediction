a7349292139021bf6e4841f64615f14ddd505d7b
RuiVelho <ruimv93@gmail.com>
2023-05-24 17:57:35 +0100

    Updates Streamlit Page Test

--- streamlit_test.py
+++ streamlit_test.py
@@ -28,7 +28,6 @@ table_actor_10_top = pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-
 table_actor_10_tail = pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/table_actor_10_tail.csv')
 
 genres_analysis=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/genres_analysis.csv')
-
 words=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/genres_2205.csv')
 
 action=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Action.csv')
@@ -46,11 +45,11 @@ filmnoir=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review
 game=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Game-Show.csv')
 history=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/History.csv')
 horror=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Horror.csv')
-music=pd.read_csv('https://github.com/RuiVelho/My-movie-review-prediction/blob/main/Music.csv')
+music=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Music.csv')
 musical=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Musical.csv')
 mystery=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Mystery.csv')
 news=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/News.csv')
-reality=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Reality-TV.csv')
+realitv=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Reality-TV.csv')
 romance=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Romance.csv')
 scifi=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Sci-Fi.csv')
 short=pd.read_csv('https://raw.githubusercontent.com/RuiVelho/My-movie-review-prediction/main/Short.csv')
@@ -104,7 +103,7 @@ if dash == "Actors":
     st.pyplot(fig2)
     
 if dash == 'Most common words in the Titles in each Category':
-    category = st.sidebar.radio("Select the Category:", 
+    category = st.selectbox("Select the Category:", 
     options=words['genre'])
     if category == words['genre'].iloc[0]:
         st.subheader('Most Common Words in Titles of Drama Movies')
@@ -124,7 +123,7 @@ if dash == 'Most common words in the Titles in each Category':
             drama
             
     
-     if category == words['genre'].iloc[1]:
+    if category == words['genre'].iloc[1]:
         st.subheader('Most Common Words in Titles of Comedy Movies')
         col1, col2 = st.columns(2)
         with col1:
@@ -180,7 +179,7 @@ if dash == 'Most common words in the Titles in each Category':
         col1, col2 = st.columns(2)
         with col1:
             adventure_counts = dict(zip(adventure['words'], adventure['count']))
-            wordcloud4 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud4 = WordCloud(width=850, height=850, max_font_size=250, min_font_size=20, background_color = 'white')
             wordcloud4.generate_from_frequencies(adventure_counts)
             plt.figure()
             plt.imshow(wordcloud4, interpolation="bilinear")
@@ -293,8 +292,314 @@ if dash == 'Most common words in the Titles in each Category':
         
         with col2:
             fantasy
+
+    if category == words['genre'].iloc[11]:
+        st.subheader('Most Common Words in Titles of Sci-Fi Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            scifi_counts = dict(zip(scifi['words'], scifi['count']))
+            wordcloud11 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud11.generate_from_frequencies(scifi_counts)
+            plt.figure()
+            plt.imshow(wordcloud11, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud11.png")
+            st.image("wordcloud11.png")
             
-   
+        with col2:
+            scifi
+            
+    if category == words['genre'].iloc[12]:
+        st.subheader('Most Common Words in Titles of Biography Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            biog_counts = dict(zip(biog['words'], biog['count']))
+            wordcloud12 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud12.generate_from_frequencies(biog_counts)
+            plt.figure()
+            plt.imshow(wordcloud12, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud12.png")
+            st.image("wordcloud12.png")
+            
+        with col2:
+            biog
+            
+    if category == words['genre'].iloc[13]:
+        st.subheader('Most Common Words in Titles of Family Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            family_counts = dict(zip(family['words'], family['count']))
+            wordcloud13 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud13.generate_from_frequencies(family_counts)
+            plt.figure()
+            plt.imshow(wordcloud13, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud13.png")
+            st.image("wordcloud13.png")
+            
+        with col2:
+            family
+            
+    if category == words['genre'].iloc[14]:
+        st.subheader('Most Common Words in Titles of History Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            history_counts = dict(zip(history['words'], history['count']))
+            wordcloud14 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud14.generate_from_frequencies(history_counts)
+            plt.figure()
+            plt.imshow(wordcloud14, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud14.png")
+            st.image("wordcloud14.png")
+            
+        with col2:
+            history           
+            
+    if category == words['genre'].iloc[15]:
+        st.subheader('Most Common Words in Titles of Music Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            music_counts = dict(zip(music['words'], music['count']))
+            wordcloud15 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud15.generate_from_frequencies(music_counts)
+            plt.figure()
+            plt.imshow(wordcloud15, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud15.png")
+            st.image("wordcloud15.png")
+            
+        with col2:
+            music
+
+    if category == words['genre'].iloc[15]:
+        st.subheader('Most Common Words in Titles of Music Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            music_counts = dict(zip(music['words'], music['count']))
+            wordcloud15 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud15.generate_from_frequencies(music_counts)
+            plt.figure()
+            plt.imshow(wordcloud15, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud15.png")
+            st.image("wordcloud15.png")
+            
+        with col2:
+            music
+            
+    if category == words['genre'].iloc[16]:
+        st.subheader('Most Common Words in Titles of Documentary Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            documentary_counts = dict(zip(documentary['words'], documentary['count']))
+            wordcloud16 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud16.generate_from_frequencies(documentary_counts)
+            plt.figure()
+            plt.imshow(wordcloud16, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud16.png")
+            st.image("wordcloud16.png")
+            
+        with col2:
+            documentary
+            
+    if category == words['genre'].iloc[17]:
+        st.subheader('Most Common Words in Titles of War Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            war_counts = dict(zip(war['words'], war['count']))
+            wordcloud17 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud17.generate_from_frequencies(war_counts)
+            plt.figure()
+            plt.imshow(wordcloud17, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud17.png")
+            st.image("wordcloud17.png")
+            
+        with col2:
+            war
+            
+    if category == words['genre'].iloc[18]:
+        st.subheader('Most Common Words in Titles of Sport Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            sports_counts = dict(zip(sports['words'], sports['count']))
+            wordcloud18 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud18.generate_from_frequencies(sports_counts)
+            plt.figure()
+            plt.imshow(wordcloud18, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud18.png")
+            st.image("wordcloud18.png")
+            
+        with col2:
+            sports
+            
+    if category == words['genre'].iloc[19]:
+        st.subheader('Most Common Words in Titles of Musical Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            musical_counts = dict(zip(musical['words'], musical['count']))
+            wordcloud19 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud19.generate_from_frequencies(musical_counts)
+            plt.figure()
+            plt.imshow(wordcloud19, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud19.png")
+            st.image("wordcloud19.png")
+            
+        with col2:
+            musical
+            
+    if category == words['genre'].iloc[20]:
+        st.subheader('Most Common Words in Titles of Western Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            western_counts = dict(zip(western['words'], western['count']))
+            wordcloud20 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud20.generate_from_frequencies(western_counts)
+            plt.figure()
+            plt.imshow(wordcloud20, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud20.png")
+            st.image("wordcloud20.png")
+            
+        with col2:
+            western
+            
+    if category == words['genre'].iloc[21]:
+        st.subheader('Most Common Words in Titles of Film-Noir Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            filmnoir_counts = dict(zip(filmnoir['words'], filmnoir['count']))
+            wordcloud21 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud21.generate_from_frequencies(filmnoir_counts)
+            plt.figure()
+            plt.imshow(wordcloud21, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud21.png")
+            st.image("wordcloud21.png")
+            
+        with col2:
+            filmnoir
+            
+    if category == words['genre'].iloc[22]:
+        st.subheader('Most Common Words in Titles of Short Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            short_counts = dict(zip(short['words'], short['count']))
+            wordcloud22 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud22.generate_from_frequencies(short_counts)
+            plt.figure()
+            plt.imshow(wordcloud22, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud22.png")
+            st.image("wordcloud22.png")
+            
+        with col2:
+            short
+            
+    if category == words['genre'].iloc[23]:
+        st.subheader('Most Common Words in Titles of Reality Tv')
+        col1, col2 = st.columns(2)
+        with col1:
+            realitv_counts = dict(zip(realitv['words'], realitv['count']))
+            wordcloud23 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud23.generate_from_frequencies(realitv_counts)
+            plt.figure()
+            plt.imshow(wordcloud23, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud23.png")
+            st.image("wordcloud23.png")
+            
+        with col2:
+            realitv
+            
+    if category == words['genre'].iloc[24]:
+        st.subheader('Most Common Words in Titles of Talk Shows')
+        col1, col2 = st.columns(2)
+        with col1:
+            talk_counts = dict(zip(talk['words'], talk['count']))
+            wordcloud24 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud24.generate_from_frequencies(talk_counts)
+            plt.figure()
+            plt.imshow(wordcloud24, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud24.png")
+            st.image("wordcloud24.png")
+            
+        with col2:
+            talk
+            
+    if category == words['genre'].iloc[25]:
+        st.subheader('Most Common Words in Titles of Games Shows')
+        col1, col2 = st.columns(2)
+        with col1:
+            game_counts = dict(zip(game['words'], game['count']))
+            wordcloud25 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud25.generate_from_frequencies(game_counts)
+            plt.figure()
+            plt.imshow(wordcloud25, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud25.png")
+            st.image("wordcloud25.png")
+            
+        with col2:
+            game
+            
+    if category == words['genre'].iloc[26]:
+        st.subheader('Most Common Words in Titles of News')
+        col1, col2 = st.columns(2)
+        with col1:
+            news_counts = dict(zip(news['words'], news['count']))
+            wordcloud26 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud26.generate_from_frequencies(news_counts)
+            plt.figure()
+            plt.imshow(wordcloud26, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud26.png")
+            st.image("wordcloud26.png")
+            
+        with col2:
+            news
+            
+    if category == words['genre'].iloc[27]:
+        st.subheader('Most Common Words in Titles of Adults Movies')
+        col1, col2 = st.columns(2)
+        with col1:
+            adult_counts = dict(zip(adult['words'], adult['count']))
+            wordcloud27 = WordCloud(width=700, height=700, max_font_size=300, min_font_size=20, background_color = 'white')
+            wordcloud27.generate_from_frequencies(adult_counts)
+            plt.figure()
+            plt.imshow(wordcloud27, interpolation="bilinear")
+            plt.axis("off")
+            plt.margins(x=0, y=0)
+            plt.savefig("wordcloud27.png")
+            st.image("wordcloud27.png")
+            
+        with col2:
+            adult
+            
+            
     #st.write(words[words['genre']==category]['count words'][0])
     #text=words[words['genre']==category]['count words'][0]
    
